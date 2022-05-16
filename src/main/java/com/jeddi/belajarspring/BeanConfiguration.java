025d3c7a3b1f2bf0ace4d69b9f2f284a85e82b66
Jeddi212 <jedediahf51@gmail.com>
2022-05-16 12:31:50 +0700

    Get Bean

--- src/main/java/com/jeddi/belajarspring/BeanConfiguration.java
+++ src/main/java/com/jeddi/belajarspring/BeanConfiguration.java
@@ -1,14 +1,17 @@
 package com.jeddi.belajarspring;
 
 import com.jeddi.belajarspring.data.Foo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Slf4j
 @Configuration
 public class BeanConfiguration {
 
     @Bean
     public Foo foo() {
+        log.info("Create new foo");
         return new Foo();
     }
 }
