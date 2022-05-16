9780e721b0dfcd2e0881f392a5049f20d195e1a2
Jeddi212 <jedediahf51@gmail.com>
2022-05-16 14:07:21 +0700

    Custom Scope ::: Doubletom

--- src/main/java/com/jeddi/belajarspring/ScopeConfiguration.java
+++ src/main/java/com/jeddi/belajarspring/ScopeConfiguration.java
@@ -1,7 +1,10 @@
 package com.jeddi.belajarspring;
 
+import com.jeddi.belajarspring.data.Bar;
 import com.jeddi.belajarspring.data.Foo;
+import com.jeddi.belajarspring.scope.DoubletonScope;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.config.CustomScopeConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -10,11 +13,24 @@ import org.springframework.context.annotation.Scope;
 @Configuration
 public class ScopeConfiguration {
 
-
     @Bean
     @Scope("prototype")
     public Foo foo() {
         log.info("Create new Foo");
         return new Foo();
+    }
+
+    @Bean
+    public CustomScopeConfigurer customScopeConfigurer() {
+        CustomScopeConfigurer configurer = new CustomScopeConfigurer();
+        configurer.addScope("doubleton", new DoubletonScope());
+        return configurer;
+    }
+
+    @Bean
+    @Scope("doubleton")
+    public Bar bar() {
+        log.info("Create new Bar");
+        return new Bar();
     }
 }
