da1c76e2669cac711438389825643fb0f945b1af
Jeddi212 <jedediahf51@gmail.com>
2022-05-16 13:42:33 +0700

    Lazy

--- src/test/java/com/jeddi/belajarspring/DependsOnTest.java
+++ src/test/java/com/jeddi/belajarspring/DependsOnTest.java
@@ -1,5 +1,6 @@
 package com.jeddi.belajarspring;
 
+import com.jeddi.belajarspring.data.Foo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
@@ -16,5 +17,6 @@ public class DependsOnTest {
 
     @Test
     void testDependsOn() {
+        Foo foo = applicationContext.getBean(Foo.class);
     }
 }
